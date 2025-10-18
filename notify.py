45a202872efeae061f0556e923ef90cdf4126dc7
mhmdkrtm <mhero0030@gmail.com>
2025-10-18 20:15:56 +0300

    Refactor Telegram token and chat ID variables

--- notify.py
+++ notify.py
@@ -1,16 +1,16 @@
 import os
 import requests
 
-TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
-TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
+TG_TOKEN = os.getenv("TG_TOKEN")
+CHAT_ID = os.getenv("CHAT_ID")
 
 def send_message(message: str):
-    if not TELEGRAM_TOKEN or not TELEGRAM_CHAT_ID:
+    if not TG_TOKEN or not CHAT_ID:
         print(f"💬 {message}")
         return
 
-    url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage"
-    payload = {"chat_id": TELEGRAM_CHAT_ID, "text": message}
+    url = f"https://api.telegram.org/bot{TG_TOKEN}/sendMessage"
+    payload = {"chat_id": CHAT_ID, "text": message}
     try:
         requests.post(url, data=payload)
     except Exception as e:
